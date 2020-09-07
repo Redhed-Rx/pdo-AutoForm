1f65ced370071b605c37880054124c4941f18caf
Redhed-Rx <63240919+Redhed-Rx@users.noreply.github.com>
2020-09-07 16:40:48 +0200

    maj

--- listNationality.php
+++ listNationality.php
@@ -2,11 +2,25 @@
 include "header.php";
 include "connexionPDO.php";
 //sql=>req
-$sql=$bdd->prepare("SELECT * FROM nationalite");
+$sql=$bdd->prepare("SELECT n.num, n.libelle, c.libelle from nationalite n, contient c where n.numContinent = c.num ");
 $sql->setFetchMode(PDO::FETCH_OBJ);
 $sql->execute();
-
 $allNationality=$sql->fetchAll();
+
+if(!empty($_SESSION['message'])){
+    $mesMessages = $_SESSION['message'];
+    foreach($mesMessages as $key=>$message){
+        echo '<div class="container pt-5">
+            <div class="alert alert-'.$key.' alert-dismissible fade show" role="alert">'.$message.'
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+              </div>';
+    }
+    $_SESSION['message']=[];
+}
+
 ?>
 <div class="container mt-5">
     <div class="row pt-3">
@@ -18,7 +32,8 @@ $allNationality=$sql->fetchAll();
         <thead class="thead-light" >
             <tr class="d-flex">
                 <th scope="col" class="col-md-2">Numéro</th>
-                <th scope="col" class="col-md-8">Libellé</th>
+                <th scope="col" class="col-md-5">Libellé</th>
+                <th scope="col" class="col-md-3">continent</th>
                 <th scope="col" class="col-md-2">Actions</th>
             </tr>
         </thead>
@@ -27,6 +42,7 @@ $allNationality=$sql->fetchAll();
             foreach($allNationality as $nationality){
                 echo "<tr class=\"d-flex\">";
                 echo "<td class=\"col-md-2\">$nationality->num</td>";
+                echo "<td class=\"col-md-8\">$nationality->libelle</td>";
                 echo "<td class=\"col-md-8\">$nationality->libelle</td>";
                 echo "<td class=\"col-md-2\">
                     <a href=\"formNationality.php?action=Modifier&num=$nationality->num\" class=\"btn btn-info\"><i class=\"fas fa-pen\"></i></a>
--- supprNationality.php
+++ supprNationality.php
@@ -1,5 +1,4 @@
-<?php 
-include "header.php";
+<?php session_start();
 include "connexionPDO.php";
 $num=$_GET['num'];
 
@@ -7,18 +6,20 @@ $sql=$bdd->prepare("delete from nationalite where num = :num");
 $sql->bindParam(':num', $num);
 $nb=$sql->execute();
 
-echo '<div class="container mt-5 pt-3">';
-echo '<div class="row"> <div class="col mt-5">';
+//echo '<div class="container mt-5 pt-3">';
+//echo '<div class="row"> <div class="col mt-5">';
 
 if($nb==1){
-    echo '<div class="alert alert-success" role="alert">La nationalité a bien été supprimée </div>' ;
+    $_SESSION['message']=['success'=>"La nationalité a bien été supprimée "];
 }else{
-    echo '<div class="alert alert-danger" role="alert">Petit problème : La nationalité n\'a pas été supprimée </div>' ;
+    $_SESSION['message']=['danger'=>"Petit problème : La nationalité n'a pas été supprimée "];
 }
+header('location: listNationality.php');
+exit();
 ?>
 </div>
 </div>
-<a href="listNationality.php" class="btn btn-primary"> Revenir à la liste des nationalités</a>
-</div>
-
-<?php include "footer.php";?>
+<!--<a href="listNationality.php" class="btn btn-primary"> Revenir à la liste des nationalités</a>-->
+<!--</div>-->
+<!--<-->
+<!--<?php // include "footer.php";?>-->
